aa8237d721bd1b5ce7b3861527c9963aba9a7fec
VISHAL SINGH <daggercoding@gmail.com>
2024-03-22 14:38:26 +0530

    created home page component to add products

---
+++ src/Components/Homepage/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <img src="https://cdn.dummyjson.com/product-images/1/thumbnail.jpg" alt="" />
+    </div>
+  )
+}
+
+export default Card
---
+++ src/Components/Homepage/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './Card'
+
+const Home = () => {
+  return (
+    <div className='mt-2'>
+        <h1 className='text-2xl font-bold'>Buy & Sell at Same Place</h1>
+        <Card></Card>
+    </div>
+  )
+}
+
+export default Home
