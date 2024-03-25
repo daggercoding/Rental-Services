6a5ba1d53aee84a19b035efd5188dfc73a6f0cbd
VISHAL SINGH <daggercoding@gmail.com>
2024-03-25 23:20:14 +0530

    manupulating the data using context api

--- src/Components/Utils/Context.jsx
+++ src/Components/Utils/Context.jsx
@@ -1,12 +1,24 @@
-import {createContext} from "react";
+import {createContext, useState,useEffect} from "react";
 
 export const RandomContext = createContext()
 
 function Context ({children}){
 
+   let [filterData,setFilterData]=useState("")
+   let [datac,setData]=useState([])
+   let [checkList,setCheckList]=useState([])
+   
+  useEffect(()=>{
+    fetch("http://localhost:8000/getitems")
+    .then((resp)=>{
+      return resp.json()})
+    .then((item)=>{setData([...item.data.reverse()])})
+    .catch((err)=>console.log(err.message))
+  },[])
+
    return(
    <>
-   <RandomContext.Provider value={{name:"vishal"}}>
+   <RandomContext.Provider value={{datac,filterData,setFilterData,setCheckList,checkList}}>
     {children}
    </RandomContext.Provider>
    </>
