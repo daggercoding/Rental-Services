b1dced932d1f7bbebd992e08d7674c2b9e59dda7
VISHAL SINGH <daggercoding@gmail.com>
2024-03-27 23:41:58 +0530

    handled crash in backend for multiple requests at a time

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,28 +1,35 @@
 const express =require("express")
-const {userDetail,userItem} = require("./Model/Db")
+const {userDetail,userItem,CartItem} = require("./Model/Db")
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken")
 const env = require("dotenv")
-const Cookie = require("cookie-parser")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+const endpoint=require("./Endpoint")
 
 const app = express()
+app.use(cors())
 env.config()
-app.use(Cookie())
+app.use(cookieParser())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}))
 
 ////=================================>>>> Creating Routes to Handle Form Data
-app.get("/",(req,res)=>{
-    res.send("hello ji")
-    res.redirect()
-})
+app.get("/",endpoint.sayHello)
 
 app.post("/login",async(req,res)=>{
    const validUser=await userDetail.find({name:req.body.name,password:req.body.password})
-   if(validUser.length<0)
+   if(validUser.length<=0)
    {
-    res.redirect("http://localhost:3000/#")
+    res.redirect("http://localhost:3000/login")
    }
-   res.redirect("http://localhost:3000/#")
+   else{
+    const token = jwt.sign({_id:validUser[0]._id},process.env.SECRET,{
+        expiresIn:process.env.EXPIRE
+    }) 
+    res.cookie("token",token)
+    res.redirect("http://localhost:3000")
+   }
 })
 
 
@@ -36,15 +43,15 @@ app.post("/signup",async(req,res)=>{
     const token=jwt.sign({_id:user._id},process.env.SECRET,{
         expiresIn:process.env.EXPIRE
     })
-    res.cookie("Token",token,{ httpOnly: true})
+    res.cookie("token",token,{ httpOnly: true})
     res.redirect("http://localhost:3000/")
 })
 
 app.post("/addItem",async(req,res)=>{
-    console.log(req.body)
     const newItem = new userItem({
         product:req.body.pname,
         link:req.body.link,
+        description:req.body.description,
         charges:req.body.price,
         duration:req.body.duration,
         quantity:req.body.quantity
@@ -54,6 +61,56 @@ app.post("/addItem",async(req,res)=>{
     res.redirect("http://localhost:3000/")
 })
 
+////=================================>>>>creating route to send data to the user
+
+app.get("/getitems",async(req,res)=>{
+    const data = await userItem.find()
+    res.status(200).json({
+        data
+    })
+})
+////=================================>>>>creating route to Save Filter Data by the USer
+
+app.post("/cartItem", async (req, res) => {
+    try {
+      let filterUser = new CartItem(req.body);
+      let allData = await CartItem.find();
+      let condition = allData.filter((item) => item._id == req.body._id);
+      if (condition.length == 0) {
+        await filterUser.save();
+        res.status(200).json({ status:"Success"});
+      }else{
+        res.status(400).json({status:"Item Already Exist"})
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  });
+  
+  // Route to retrieve all cart items
+  app.get("/cartItems", async (req, res) => {
+    try {
+      let data = await CartItem.find();
+      res.status(200).json({
+        status: "Success",
+        data
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  });
+
+////=================================>>>>creating route to Delete Filter Data by the USer
+app.delete("/deleteCart",async(req,res)=>{
+    const deletedItem=await CartItem.findByIdAndDelete(req.body.id)
+    res.status(200).send({
+        data:deletedItem
+    })
+})
+
+
 app.listen(8000,()=>{
-    console.log("Server is Up Baby :)")
+    console.log("Server is Up Baby..:)")
 })
