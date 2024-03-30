c325816a42dec10e3ec6c3cc6a0f9dd65bf7ff3a
VISHAL SINGH <daggercoding@gmail.com>
2024-03-30 23:34:56 +0530

    replaces normal form action and method with fetch to handle api call

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,12 +1,42 @@
-import { Link } from "react-router-dom"
+import { Link, Navigate, useNavigate } from "react-router-dom"
+import { useContext, useState } from "react"
+import { RandomContext } from "./Utils/Context"
 
 function Login(){
+    const Navigate =useNavigate()
+    let [name,setName]=useState("")
+    let [password,setPassword]=useState("")
+    let  {setLogin} = useContext(RandomContext)
+
+    function handleClick(e){
+        e.preventDefault()
+        const user={
+            name,
+            password
+        }
+        fetch("http://localhost:8000/login",{
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(user)
+        })
+        .then(resp=>resp.json())
+        .then(data=>{
+            console.log(data)
+            if(data.length===1){
+                localStorage.setItem("token",JSON.stringify(data[0].id))
+                setLogin(true)
+                Navigate("/")
+            }
+            })
+        .catch(err=>err)
+    }
+  
     return(  
-        <form  action="http://localhost:8000/login" method="POST" className="flex flex-col w-[50vw] mt-20 border-2 p-10 rounded items-center " >
-         <h1 className="text-3xl antialiased font-bold">LOGIN</h1>
-         <input type="text" name="name" placeholder="UserName"/>
-         <input type="password" name="password" placeholder="Password"/>
-         <button className="bg-black text-white w-[50%] p-2 rounded mt-4 border-gray-500 border-[0.5px] " type="submit">Login</button>
+        <form className="flex flex-col w-[50vw] mt-20 border-2 p-10 rounded items-center " >
+         <h1 className="text-2xl antialiased font-bold sm:text-3xl">LOGIN</h1>
+         <input id="name"  type="text" name="name" placeholder="UserName" onChange={(e)=>setName(e.target.value)}/>
+         <input id="passoword"  type="password" name="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
+         <button className="bg-black text-white w-[50%] p-2 rounded mt-4 border-gray-500 border-[0.5px] " onClick={handleClick} type="submit">Login</button>
          <p className="mt-2">Dont't Have Account? <Link to="/signup" className="text-blue-400"> Please SignUp</Link></p>
         </form>
     )
