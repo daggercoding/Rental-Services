cf03f62350fc81f87fcca7b3e1738fb6807d1885
VISHAL SINGH <daggercoding@gmail.com>
2024-05-06 21:26:53 +0530

    creates 2 endpoints to handle and verify payments

--- Backend/Server.js
+++ Backend/Server.js
@@ -4,6 +4,9 @@ const env = require("dotenv")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
 const endpoint=require("./Controller/Endpoint")
+const Razorpay = require("razorpay")
+const crypto = require("crypto");
+const { userDetail } = require("./Model/Db");
 
 
 const app = express()
@@ -32,7 +35,53 @@ app.post("/singleUser",endpoint.singleUser)
 
 app.delete("/deleteCart",endpoint.deleteCart)
 
+app.post("/order", async(req, res)=>{
+    try{
+        const razorpay = new Razorpay({
+            key_id:process.env.RAZORPAYID,
+            key_secret:process.env.RAZORPAYKEY
+        })
+    
+        const option = req.body
+        const order = await razorpay.orders.create(option)
+        if(!order){
+            res.status(500).send("some error occured")
+        }
+        res.status(200).json(order)
+    }catch(err){
+        console.log(err)
+    }
+})
 
+app.post("/validatePayment",(req,res)=>{
+    try{
+        
+        const {razorpay_order_id,razorpay_payment_id,razorpay_signature}=req.body
+        const sha  = crypto.createHmac("sha256",process.env.RAZORPAYKEY)
+        sha.update(`${razorpay_order_id}|${razorpay_payment_id}`)
+        const digest = sha.digest("hex")
+        if(digest!==razorpay_signature){
+            return res.status(400).json({msg:"Transaction is not Legit"})
+        }
+        res.json({
+            status:"Sucess",
+            orderId:razorpay_order_id,
+            paymentId:razorpay_payment_id
+        })
+    }catch(err){
+        console.log(err)
+    }
+   
+})
+
+app.delete("/emptyCart", async (req, res) =>{
+    const id = req.body.ids;
+
+   let deltedData = await userDetail.findOneAndUpdate({_id:id}, { $unset:{"cart":1}},{new:false} );
+   console.log(deltedData)
+    res.status(200).json({status:"success", data:deltedData});
+
+})
 app.listen(8000,()=>{
     console.log("Server is Up Baby..:)")
 })
