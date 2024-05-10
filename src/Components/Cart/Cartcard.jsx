b8bc08418e0909985b606ee15ab3c98dfcb9339d
VISHAL SINGH <daggercoding@gmail.com>
2024-05-10 23:40:32 +0530

    modified to update the total cart value

--- src/Components/Cart/Cartcard.jsx
+++ src/Components/Cart/Cartcard.jsx
@@ -1,39 +1,43 @@
-import React, { useState } from "react";
-export default function Cartcard({product, handleClick}) {
-    console.log(product.quantity)
-    const [add, setAdd] = useState(1);
-    const [available,setAvailable]= useState(true)
-    console.log("rerendered")
-    function increment(){ 
+import React, { useEffect, useState } from "react";
 
-        if(add>=product.quantity)
-        {
-          setAvailable(false)
-          setAdd(prev => prev+1)
-          setTimeout(()=>{
-            setAvailable(true)
-            setAdd(prev => prev-1)
-          },2000)
-         
-        }else{
-          setAdd(prev => prev+1)
-        } 
-      }
-      function decrement(){
-        if(add>=1){
-          setAvailable(true)
-        }
+export default function Cartcard({ product, handleClick, setTotal }) {
+  const [add, setAdd] = useState(1);
+  const [available, setAvailable] = useState(true);
 
-        if(add==1){
-          setAdd(1);
-        }else{
-          setAdd(prev => prev -1)
-        }
-        
-      }
-    
+  useEffect(()=>{
+   let number=JSON.parse(localStorage.getItem("vishal"))??1
    
-    
+  },[])
+ 
+  function increment(price,id) {
+    if (add >= product.quantity) {
+      setAvailable(false);
+      setAdd((prev) => prev + 1);
+      setTimeout(() => {
+        setAvailable(true);
+        setAdd((prev) => prev - 1);
+      }, 2000);
+    } else {
+      setAdd((prev) => prev + 1);
+      setTotal((prev) => prev + price);
+      JSON.stringify(localStorage.setItem("vishal",add))
+
+    }
+  }
+
+  function decrement(price) {
+    if (add >= 1) {
+      setAvailable(true);
+    }
+
+    if (add == 1) {
+      setAdd(1);
+    } else {
+      setAdd((prev) => prev - 1);
+      setTotal((prev) => prev - price);
+    }
+  }
+
   return (
     <div className="w-[100%] h-36 flex border-white border-[0.5px] mb-2 rounded-md bg-neutral-800 text-white p-1">
       <div className="w-[30%]  border-[1px] border-white ">
@@ -45,9 +49,11 @@ export default function Cartcard({product, handleClick}) {
           <span>{product.charges * add} $</span>
         </p>
         <p className="flex justify-between w-[100%] p-2">
-          <span className="text-green-300">{available?"InStock":"OutOfStock"}</span>
+          <span className="text-green-300">
+            {available ? "InStock" : "OutOfStock"}
+          </span>
           <button
-          onClick={()=>handleClick(product._id)}
+            onClick={() => handleClick(product._id)}
             className="text-white border-white border-[0.5px] p-1 rounded-s transition-all cursor-pointer"
           >
             {" "}
@@ -56,14 +62,14 @@ export default function Cartcard({product, handleClick}) {
         </p>
         <p className="ml-2 flex">
           <button
-            onClick={decrement}
+            onClick={() => decrement(product.charges)}
             className=" w-8 h-8 mr-1 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded"
           >
             -
           </button>
           <p className="mx-2 w-4">{add} </p>
           <button
-            onClick={increment}
+            onClick={() => increment(product.charges,product._id)}
             className=" w-8 h-8 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded"
           >
             +
